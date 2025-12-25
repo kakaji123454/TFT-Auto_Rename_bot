b0f04be7ecf31b2976562087c72451e635c07f14
kakaji123454 <k73931307@gmail.com>
2025-12-25 23:41:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,15 +17,15 @@ USER_LIMIT_TIME = int(os.environ.get("USER_LIMIT_TIME", "1"))#enter time based o
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", " ")
-    API_HASH  = os.environ.get("API_HASH", "")
+    API_ID    = os.environ.get("API_ID", "26331872")
+    API_HASH  = os.environ.get("API_HASH", "c93589620441707c37c5683a02eea54e")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
 
     # database config
-    DB_NAME = os.environ.get("DB_NAME","")     
-    DB_URL  = os.environ.get("DB_URL","")
+    DB_NAME = os.environ.get("DB_NAME","VJ_Botz")     
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://ajmerasaini01:U1sGiZRI6Ha0xuCy@cluster0.tnok3d0.mongodb.net/")
     PORT = os.environ.get("PORT", "8050")
-    OWNER = int(os.environ.get("OWNER", ""))
+    OWNER = int(os.environ.get("OWNER", "8413546109"))
     PRIVATE_USE = False #If Bot is private use set True otherwise False
  
     # other configs
@@ -33,8 +33,8 @@ class Config(object):
     START_PIC   = os.environ.get("START_PIC", "https://files.catbox.moe/ufzpkn.jpg")
     
     FORCE_SUB_CHANNELS = os.environ.get('FORCE_SUB_CHANNELS', "tech_freak_tamil").split(',')
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "" ))
-    DUMB_CHANNEL = os.environ.get("DUMB_CHANNEL", "")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003161993313" ))
+    DUMB_CHANNEL = os.environ.get("DUMB_CHANNEL", "-1003161993313")
     
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
